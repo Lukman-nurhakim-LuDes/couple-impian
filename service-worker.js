37b068af2e29f94d7b00024bc985f3f37c005ef4
Lukman <lukmannrhkm80@gmail.com>
2025-07-24 16:41:44 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,22 +4,27 @@ const urlsToCache = [
     '/index.html',
     '/app.js',
     '/manifest.json',
+    'https://unpkg.com/react@18/umd/react.production.min.js',
+    'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+    'https://cdn.tailwindcss.com',
+    'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap',
     // Tambahkan URL ikon yang digunakan di sini agar bisa di-cache
-    'http://googleusercontent.com/file_content/0?contentFetchId=uploaded:Dasboard.png-c08212f9-e382-4ea6-aad3-963ea6c21cfc',
-    'http://googleusercontent.com/file_content/0?contentFetchId=uploaded:Keuangan.png-88ae531d-2354-473f-8df5-182c9446e253',
-    'http://googleusercontent.com/file_content/0?contentFetchId=uploaded:Tabungan.png-9e41643e-d7ef-41cc-9cee-838dbbbf2e5c',
-    'http://googleusercontent.com/file_content/0?contentFetchId=uploaded:Game-time.png-e6a68a2b-d98b-48fa-b24f-af92b70f0145',
-    'http://googleusercontent.com/file_content/0?contentFetchId=uploaded:pasangan.jpg-74ef9965-99b5-47a2-a44f-18dfdd16b132',
-    'http://googleusercontent.com/file_content/0?contentFetchId=uploaded:Calender.png-f713adef-8c8c-48fe-a163-3a27626232ad',
-    'http://googleusercontent.com/file_content/0?contentFetchId=uploaded:Habits.png-3847b4bc-8889-43c7-a2ba-7193ea1971b8',
-    'http://googleusercontent.com/file_content/0?contentFetchId=uploaded:pic-drop.png-2cbb69f5-bcd3-419f-9582-465ecba93840',
-    'http://googleusercontent.com/file_content/0?contentFetchId=uploaded:wishlist.png-8c35dc3c-0673-4b4f-bc0e-de088a0a933b',
-    'http://googleusercontent.com/file_content/0?contentFetchId=uploaded:Monitor-event.png-b7009ade-dad1-43ec-8363-29fefbd77e9d',
-    'http://googleusercontent.com/file_content/0?contentFetchId=uploaded:aku.png-7e124c76-1de2-454b-bef5-1c9fbb1bfa18',
-    'http://googleusercontent.com/file_content/0?contentFetchId=uploaded:kamu.png-59ac26c5-fe8e-4961-9f2f-8fd1e1efc913',
+    'https://googleusercontent.com/file_content/0?contentFetchId=uploaded:Dasboard.png-c08212f9-e382-4ea6-aad3-963ea6c21cfc',
+    'https://googleusercontent.com/file_content/0?contentFetchId=uploaded:Keuangan.png-88ae531d-2354-473f-8df5-182c9446e253',
+    'https://googleusercontent.com/file_content/0?contentFetchId=uploaded:Tabungan.png-9e41643e-d7ef-41cc-9cee-838dbbbf2e5c',
+    'https://googleusercontent.com/file_content/0?contentFetchId=uploaded:Game-time.png-e6a68a2b-d98b-48fa-b24f-af92b70f0145',
+    'https://googleusercontent.com/file_content/0?contentFetchId=uploaded:pasangan.jpg-74ef9965-99b5-47a2-a44f-18dfdd16b132',
+    'https://googleusercontent.com/file_content/0?contentFetchId=uploaded:Calender.png-f713adef-8c8c-48fe-a163-3a27626232ad',
+    'https://googleusercontent.com/file_content/0?contentFetchId=uploaded:Habits.png-3847b4bc-8889-43c7-a2ba-7193ea1971b8',
+    'https://googleusercontent.com/file_content/0?contentFetchId=uploaded:pic-drop.png-2cbb69f5-bcd3-419f-9582-465ecba93840',
+    'https://googleusercontent.com/file_content/0?contentFetchId=uploaded:wishlist.png-8c35dc3c-0673-4b4f-bc0e-de088a0a933b',
+    'https://googleusercontent.com/file_content/0?contentFetchId=uploaded:Monitor-event.png-b7009ade-dad1-43ec-8363-29fefbd77e9d',
+    'https://googleusercontent.com/file_content/0?contentFetchId=uploaded:aku.png-7e124c76-1de2-454b-bef5-1c9fbb1bfa18',
+    'https://googleusercontent.com/file_content/0?contentFetchId=uploaded:kamu.png-59ac26c5-fe8e-4961-9f2f-8fd1e1efc913',
     // Icon aplikasi yang baru
-    'http://googleusercontent.com/file_content/0?contentFetchId=uploaded:icon-192.png-778d07d5-a99c-4db2-b91c-87e7ecc9d4bb',
-    'http://googleusercontent.com/file_content/0?contentFetchId=uploaded:icon-512.jpg-5342d4fd-19a2-4d01-aa89-1b99e49eb33a'
+    'https://googleusercontent.com/file_content/0?contentFetchId=uploaded:icon-192.png-778d07d5-a99c-4db2-b91c-87e7ecc9d4bb',
+    'https://googleusercontent.com/file_content/0?contentFetchId=uploaded:icon-512.jpg-5342d4fd-19a2-4d01-aa89-1b99e49eb33a',
+    'https://googleusercontent.com/file_content/0?contentFetchId=uploaded:Background.jpg-009aaa04-4b2b-4921-b937-a0f717a496ec'
 ];
 
 self.addEventListener('install', (event) => {
